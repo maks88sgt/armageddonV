2f5b11206bd7b172146c44825a8754840e17ade2
maks88sgt <maks88sgt@yandex.ru>
2021-04-17 02:33:03 +0300

    Local state filter

--- src/components/AsteroidsComponent/AsteroidsHeader/AsteroidsHeader.jsx
+++ src/components/AsteroidsComponent/AsteroidsHeader/AsteroidsHeader.jsx
@@ -1,18 +1,33 @@
+import { useState} from "react";
+import classNames from "classnames";
+import './../../../styles/_distance.scss'
+
 export function AsteroidsHeader() {
+    const [onlyDangerous, setOnlyDangerous] = useState(false);
+    const [distanceInKilometers , setDistanceInKilometers] = useState(true);
+
+    const classForDistanceInKm = classNames({
+        'kilometers': distanceInKilometers,
+    })
+
+    const classForDistanceInLunar = classNames({
+        'kilometers': !distanceInKilometers,
+    })
 
 
     return (
         <>
-            <div>
+            <div onClick={() => setOnlyDangerous(!onlyDangerous)}>
                 <input type="checkbox"
                        className="styled-checkbox_input"
-                       checked={false}
-                       onChange={() => null}
+                       checked={onlyDangerous}
                 />
                 Показать только опасные
             </div>
             <div>
-                <p>Расстояние <span>в километрах</span>, <span>в дистанциях до луны</span></p>
+                <p>Расстояние <span className={classForDistanceInKm} onClick={() => setDistanceInKilometers(true)}>в километрах</span>,
+                    <span className={classForDistanceInLunar} onClick={() => setDistanceInKilometers(false)}>в дистанциях до луны</span>
+                </p>
             </div>
         </>
     );
