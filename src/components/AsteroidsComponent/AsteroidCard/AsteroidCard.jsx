5ac6f442f545b0a7e64131627a4a9d84887b422f
maks88sgt <maks88sgt@yandex.ru>
2021-04-17 15:23:25 +0300

    Context usage for distance

--- src/components/AsteroidsComponent/AsteroidCard/AsteroidCard.jsx
+++ src/components/AsteroidsComponent/AsteroidCard/AsteroidCard.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useContext} from "react";
 import classNames from "classnames";
 import './../../../styles/_card.scss';
+import {FilterContext} from "../Asteroids/Asteroids";
 
 export function AsteroidCard(props) {
     const {name, isDangerous, diameter, minRange, minRangeDate} = props;
+
+    const context = useContext(FilterContext);
+    const { distanceInKilometers } = context;
+
+    const cardDistance = distanceInKilometers ? `${minRange.kilometers} км` : `${minRange.lunar} расстояний до луны`
+
 
     const dangerousLabel = isDangerous ? 'Опасен' : 'Не опасен';
 
@@ -20,7 +27,7 @@ export function AsteroidCard(props) {
             <div className="">
                 <h5>{name}</h5>
                 <p>Дата <span>{minRangeDate}</span></p>
-                <p>Расстояние <span>{minRange.kilometers}</span></p>
+                <p>Расстояние <span>{cardDistance}</span></p>
                 <p>Размер <span>{diameter}</span></p>
             </div>
             <div className="">
