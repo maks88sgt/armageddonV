7eb89afb3460def172986ce31a9192f42071c457
maks88sgt <maks88sgt>
2022-04-26 10:10:41 +0300

    Create app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import {FunctionalComponent} from "./FunctionalComponent/FunctionalComponnt";
+import {ClassComponent} from "./ClassComponent/ClassComponent";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <FunctionalComponent/>
+      <ClassComponent/>
     </div>
   );
 }
---
+++ src/FunctionalComponent/FunctionalComponnt.jsx
@@ -0,0 +1,3 @@
+export const FunctionalComponent = ()=> {
+    return <div>Functional Component</div>;
+}
